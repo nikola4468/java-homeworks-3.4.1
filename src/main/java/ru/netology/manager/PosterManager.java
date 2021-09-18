d8df09a10e73ff63ea3b47ac2a5946ee72af4443
nikola4468 <nikola4468@gmail.com>
2021-09-18 10:30:43 +0900

    initial commit

---
+++ src/main/java/ru/netology/manager/PosterManager.java
@@ -0,0 +1,12 @@
+package ru.netology.manager;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class PosterManager {
+}
